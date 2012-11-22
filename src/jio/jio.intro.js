11b53fab463188ef2f330f5992b1ed2874a0edd7
Tristan Cavelier <tristan.cavelier@tiolive.com>
2012-11-22 16:31:13 +0100

    Add an uuid generator to jio.intro.js

--- src/jio/jio.intro.js
+++ src/jio/jio.intro.js
@@ -1,2 +1,14 @@
 var jio = function(spec) {
 
+    var generateUuid = function () {
+        var S4 = function () {
+            return Math.floor(
+                Math.random() * 0x10000 /* 65536 */
+            ).toString(16);
+        };
+        return S4() + S4() + "-" +
+            S4() + "-" +
+            S4() + "-" +
+            S4() + "-" +
+            S4() + S4() + S4();
+    };
