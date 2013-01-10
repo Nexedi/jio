dda2573d0910677d0e7256b223671d1117b457e3
Sven Franck <sven.franck@nexedi.com>
2013-01-10 14:52:12 +0100

    jslint pass postCommand.js (2indent)

--- src/jio/commands/postCommand.js
+++ src/jio/commands/postCommand.js
@@ -1,33 +1,32 @@
-var postCommand = function(spec, my) {
-    var that = command(spec, my);
-    spec = spec || {};
-    my = my || {};
-    // Attributes //
-    var priv = {};
+/*jslint indent: 2, maxlen: 80, sloppy: true */
+/*global command: true */
+var postCommand = function (spec, my) {
+  var that = command(spec, my);
 
-    // Methods //
-    that.getLabel = function() {
-        return 'post';
-    };
+  spec = spec || {};
+  my = my || {};
 
-    that.validateState = function () {
-        if (typeof that.getAttachmentId() !== "undefined") {
-            that.error({
-                "status": 21,
-                "statusText": "Invalid Document Id",
-                "error": "invalid_document_id",
-                "message": "The document id contains '/' characters "+
-                    "which are forbidden",
-                "reason": "Document id contains '/' character(s)"
-            });
-            return false;
-        }
-        return true;
-    };
+  // Methods //
+  that.getLabel = function () {
+    return 'post';
+  };
 
-    that.executeOn = function(storage) {
-        storage.post (that);
-    };
-
-    return that;
+  that.validateState = function () {
+    if (that.getAttachmentId() !== undefined) {
+      that.error({
+        "status": 21,
+        "statusText": "Invalid Document Id",
+        "error": "invalid_document_id",
+        "message": "The document id contains '/' characters " +
+          "which are forbidden",
+        "reason": "Document id contains '/' character(s)"
+      });
+      return false;
+    }
+    return true;
+  };
+  that.executeOn = function (storage) {
+    storage.post(that);
+  };
+  return that;
 };
