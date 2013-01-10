c510edfab4eed2dd27c6475ee12a9abcfcd29202
Tristan Cavelier <tristan.cavelier@tiolive.com>
2013-01-10 16:40:26 +0100

    jslint pass failStatus.js

--- src/jio/jobs/status/failStatus.js
+++ src/jio/jobs/status/failStatus.js
@@ -1,18 +1,20 @@
-var failStatus = function(spec, my) {
-    var that = jobStatus(spec, my);
-    spec = spec || {};
-    my = my || {};
-    // Attributes //
-    // Methods //
-    that.getLabel = function() {
-        return 'fail';
-    };
+/*jslint indent: 2, maxlen: 80, sloppy: true */
+/*global jobStatus: true */
+var failStatus = function (spec, my) {
+  var that = jobStatus(spec, my);
+  spec = spec || {};
+  my = my || {};
+  // Attributes //
+  // Methods //
+  that.getLabel = function () {
+    return 'fail';
+  };
 
-    that.canStart = function() {
-        return false;
-    };
-    that.canRestart = function() {
-        return true;
-    };
-    return that;
+  that.canStart = function () {
+    return false;
+  };
+  that.canRestart = function () {
+    return true;
+  };
+  return that;
 };
