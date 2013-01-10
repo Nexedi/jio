c4165e2a6051fa49ec8d1ce98f60fe55616047aa
Sven Franck <sven.franck@nexedi.com>
2013-01-10 15:14:34 +0100

    jslint pass announcement.js

--- src/jio/announcements/announcement.js
+++ src/jio/announcements/announcement.js
@@ -1,40 +1,42 @@
-var announcement = function(spec, my) {
-    var that = {};
-    spec = spec || {};
-    my = my || {};
-    // Attributes //
-    var callback_a = [];
-    var name = spec.name || '';
-    var announcer = spec.announcer || {};
-    // Methods //
-    that.add = function(callback) {
-        callback_a.push(callback);
-    };
+/*jslint indent: 2, maxlen: 80, sloppy: true */
+/*global announcement: true */
+var announcement = function (spec, my) {
+  var that = {},
+    callback_a = [],
+    announcer = spec.announcer || {};
 
-    that.remove = function(callback) {
-        var i, tmp_callback_a = [];
-        for (i = 0; i < callback_a.length; i+= 1) {
-            if (callback_a[i] !== callback) {
-                tmp_callback_a.push(callback_a[i]);
-            }
-        }
-        callback_a = tmp_callback_a;
-    };
+  spec = spec || {};
+  my = my || {};
 
-    that.register = function() {
-        announcer.register(that);
-    };
+  // Methods //
+  that.add = function (callback) {
+    callback_a.push(callback);
+  };
 
-    that.unregister = function() {
-        announcer.unregister(that);
-    };
+  that.remove = function (callback) {
+    var i, tmp_callback_a = [];
+    for (i = 0; i < callback_a.length; i += 1) {
+      if (callback_a[i] !== callback) {
+        tmp_callback_a.push(callback_a[i]);
+      }
+    }
+    callback_a = tmp_callback_a;
+  };
 
-    that.trigger = function(args) {
-        var i;
-        for(i = 0; i < callback_a.length; i++) {
-            callback_a[i].apply(null, args);
-        }
-    };
+  that.register = function () {
+    announcer.register(that);
+  };
 
-    return that;
+  that.unregister = function () {
+    announcer.unregister(that);
+  };
+
+  that.trigger = function (args) {
+    var i;
+    for (i = 0; i < callback_a.length; i += 1) {
+      callback_a[i].apply(null, args);
+    }
+  };
+
+  return that;
 };
