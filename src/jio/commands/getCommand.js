27892c94dd302540260d05645772c02c507028db
Sven Franck <sven.franck@nexedi.com>
2013-01-10 14:46:58 +0100

    jslint pass getCommand (2indent-correction)

--- src/jio/commands/getCommand.js
+++ src/jio/commands/getCommand.js
@@ -1,50 +1,50 @@
 /*jslint indent: 2, maxlen: 80, sloppy: true */
 /*global command: true */
 var getCommand = function (spec, my) {
-    var that = command(spec, my);
+  var that = command(spec, my);
 
-    spec = spec || {};
-    my = my || {};
-    // Attributes //
-    // Methods //
-    that.getLabel = function () {
-      return 'get';
-    };
+  spec = spec || {};
+  my = my || {};
+  // Attributes //
+  // Methods //
+  that.getLabel = function () {
+    return 'get';
+  };
 
-    that.validateState = function () {
-      if (!(typeof that.getDocId() === "string" &&
-        that.getDocId() !== "")) {
+  that.validateState = function () {
+    if (!(typeof that.getDocId() === "string" &&
+      that.getDocId() !== "")) {
+      that.error({
+        "status": 20,
+        "statusText": "Document Id Required",
+        "error": "document_id_required",
+        "message": "The document id is not provided",
+        "reason": "Document id is undefined"
+      });
+      return false;
+    }
+    if (typeof that.getAttachmentId() === "string") {
+      if (that.getAttachmentId() === "") {
         that.error({
-          "status": 20,
-          "statusText": "Document Id Required",
-          "error": "document_id_required",
-          "message": "The document id is not provided",
-          "reason": "Document id is undefined"
+          "status": 23,
+          "statusText": "Invalid Attachment Id",
+          "error": "invalid_attachment_id",
+          "message": "The attachment id must not be an empty string",
+          "reason": "Attachment id is empty"
         });
         return false;
       }
-      if (typeof that.getAttachmentId() === "string") {
-        if (that.getAttachmentId() === "") {
-          that.error({
-            "status": 23,
-            "statusText": "Invalid Attachment Id",
-            "error": "invalid_attachment_id",
-            "message": "The attachment id must not be an empty string",
-            "reason": "Attachment id is empty"
-          });
-          return false;
-        }
-      }
-      return true;
-    };
-
-    that.executeOn = function (storage) {
-      storage.get(that);
-    };
-
-    that.canBeRestored = function () {
-      return false;
-    };
-
-    return that;
+    }
+    return true;
   };
+
+  that.executeOn = function (storage) {
+    storage.get(that);
+  };
+
+  that.canBeRestored = function () {
+    return false;
+  };
+
+  return that;
+};
