5779a7ff5bfbc201f7cdaf77fbcfdbcf3bffe1ad
Sven Franck <sven.franck@nexedi.com>
2013-01-10 14:59:48 +0100

    jslint pass putCommand.js

--- src/jio/commands/putCommand.js
+++ src/jio/commands/putCommand.js
@@ -1,43 +1,42 @@
-var putCommand = function(spec, my) {
-    var that = command(spec, my);
-    spec = spec || {};
-    my = my || {};
-    // Attributes //
-    var priv = {};
+/*jslint indent: 2, maxlen: 80, sloppy: true */
+/*global command: true */
+var putCommand = function (spec, my) {
+  var that = command(spec, my);
+  spec = spec || {};
+  my = my || {};
 
-    // Methods //
-    that.getLabel = function() {
-        return 'put';
-    };
+  // Methods //
+  that.getLabel = function () {
+    return 'put';
+  };
 
-    that.validateState = function () {
-        if (!(typeof that.getDocId() === "string" && that.getDocId() !== "")) {
-            that.error({
-                "status": 20,
-                "statusText": "Document Id Required",
-                "error": "document_id_required",
-                "message": "The document id is not provided",
-                "reason": "Document id is undefined"
-            });
-            return false;
-        }
-        if (typeof that.getAttachmentId() !== "undefined") {
-            that.error({
-                "status": 21,
-                "statusText": "Invalid Document Id",
-                "error": "invalid_document_id",
-                "message": "The document id contains '/' characters "+
-                    "which are forbidden",
-                "reason": "Document id contains '/' character(s)"
-            });
-            return false;
-        }
-        return true;
-    };
-
-    that.executeOn = function(storage) {
-        storage.put (that);
-    };
-
-    return that;
+  that.validateState = function () {
+    if (!(typeof that.getDocId() === "string" && that.getDocId() !==
+        "")) {
+      that.error({
+        "status": 20,
+        "statusText": "Document Id Required",
+        "error": "document_id_required",
+        "message": "The document id is not provided",
+        "reason": "Document id is undefined"
+      });
+      return false;
+    }
+    if (that.getAttachmentId() !== undefined) {
+      that.error({
+        "status": 21,
+        "statusText": "Invalid Document Id",
+        "error": "invalid_document_id",
+        "message": "The document id contains '/' characters " +
+          "which are forbidden",
+        "reason": "Document id contains '/' character(s)"
+      });
+      return false;
+    }
+    return true;
+  };
+  that.executeOn = function (storage) {
+    storage.put(that);
+  };
+  return that;
 };
