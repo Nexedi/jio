750686ea4183f7007ba440a747a780a92b912ead
Sven Franck <sven.franck@nexedi.com>
2013-02-05 17:54:52 +0100

    added licence info and files

--- src/queries/begin.js
+++ src/queries/begin.js
@@ -1,3 +1,9 @@
+/*
+* Copyright 2013, Nexedi SA
+* Released under the LGPL license.
+* http://www.gnu.org/licenses/lgpl.html
+*/
+
 (function (scope) {
   "use strict";
   Object.defineProperty(scope, "ComplexQueries", {
