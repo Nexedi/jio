f7f634f28b2a89086295e3e4a62b23038b2eee8c
Tristan Cavelier <tristan.cavelier@tiolive.com>
2013-08-06 16:12:52 +0200

    revisionstorage tests added

--- test/tests.require.js
+++ test/tests.require.js
@@ -7,12 +7,15 @@
   require.config({
     "paths": {
       "md5": "../lib/md5/md5",
+      "sha256": "../lib/jsSha2/sha256",
       "complex_queries": "../complex_queries",
       "complex_queries_tests": "queries/tests",
       "jio": "../jio",
       "jio_tests": "jio/tests",
       "localstorage": "../src/jio.storage/localstorage",
       "localstorage_tests": "jio.storage/localstorage.tests",
+      "revisionstorage": "../src/jio.storage/revisionstorage",
+      "revisionstorage_tests": "jio.storage/revisionstorage.tests",
 
       "qunit": "../lib/qunit/qunit",
       "sinon": "../lib/sinon/sinon",
@@ -20,6 +23,7 @@
     },
     "shim": {
       "md5": {"exports": "hex_md5"},
+      "sha256": {"exports": "hex_sha256"},
 
       "sinon": ["qunit"],
       "sinon_qunit": ["sinon"]
@@ -30,6 +34,7 @@
     "sinon_qunit",
     "complex_queries_tests",
     "jio_tests",
-    "localstorage_tests"
+    "localstorage_tests",
+    "revisionstorage_tests"
   ]);
 }());
