ae060e624af13c060d3ed2a02de805e1aae16af5
Tristan Cavelier <tristan.cavelier@tiolive.com>
2013-08-26 12:05:16 +0200

    job rules getter added to JIO

---
+++ src/jio/core/jobRules.js
@@ -0,0 +1,20 @@
+/*jslint indent: 2, maxlen: 80, sloppy: true */
+/*global exports, defaults */
+
+// adds
+// - jIO.addJobRuleCondition(name, function)
+
+function addJobRuleCondition(name, method) {
+  if (typeof name !== 'string') {
+    throw new TypeError("jIO.addJobRuleAction(): " +
+                        "Argument 1 is not of type 'string'");
+  }
+  if (typeof method !== 'function') {
+    throw new TypeError("jIO.addJobRuleAction(): " +
+                        "Argument 2 is not of type 'function'");
+  }
+  if (defaults.job_rule_conditions[name]) {
+    throw new TypeError("jIO.addJobRuleAction(): Action already exists");
+  }
+  defaults.job_rule_conditions[name] = method;
+}
