639e5b946be493cd07b9c28688afdd88d39db9ee
Tristan Cavelier <tristan.cavelier@tiolive.com>
2013-09-02 09:51:57 +0200

    Promise.all and allOrNone notifies when a promise ended

--- src/jio/core/Promise.js
+++ src/jio/core/Promise.js
@@ -168,6 +168,7 @@ Promise.all = function (items) {
   solver = next.defer();
   function succeed(i) {
     return function (answer) {
+      solver.notify(i);
       array[i] = answer;
       count += 1;
       if (count !== items.length) {
@@ -202,6 +203,7 @@ Promise.allOrNone = function (items) {
   solver = next.defer();
   items.forEach(function (item, i) {
     Promise.when(item, function (answer) {
+      solver.notify(i);
       array[i] = answer;
       count += 1;
       if (count !== items.length) {
@@ -209,6 +211,7 @@ Promise.allOrNone = function (items) {
       }
       return solver.resolve(array);
     }, function (answer) {
+      solver.notify(i);
       return solver.reject(answer);
     });
   });
