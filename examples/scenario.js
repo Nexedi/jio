bd82b74e667bb194bb84b58c40775c850636198f
Vincent Bechu <vincent.bechu@nexedi.com>
2017-09-05 10:01:47 +0200

    Revert works on saferepair and scenarii
    This reverts commits
    215a2cca4a0d09116a7e7c7ccf3cdf9e78dff1b8
    e391b71d3e2d052d9da5a0e067e8852046ce1753
    ddf6109897ee867adcab5b81f87354db9a4c3339
    5d6e60dd997adc5d58491396d51bd1adfe50c376
    bafcf854e63cf37b5a7fb71ab604a336414d43e7

--- examples/scenario.js
+++ examples/scenario.js
@@ -1,4 +1,4 @@
-/*global console, btoa, Blob, RSVP*/
+/*global console, btoa, Blob*/
 /*jslint nomen: true, maxlen: 200*/
 (function (window, QUnit, jIO, rJS) {
   "use strict";
@@ -8,71 +8,225 @@
     ok = QUnit.ok,
     stop = QUnit.stop,
     start = QUnit.start,
-    module = QUnit.module,
-    deepEqual = QUnit.deepEqual,
-    global = {defer: RSVP.defer()};
+    deepEqual = QUnit.deepEqual;
 
   rJS(window)
 
-    .ready(function () {
+    .ready(function (g) {
 
-      global.gadget = this;
-      global.defer.resolve();
+      ///////////////////////////
+      // Local storage
+      ///////////////////////////
+//       return g.run({
+//         type: "query",
+//         sub_storage: {
+//           type: "uuid",
+//           sub_storage: {
+//             type: "document",
+//             document_id: "/",
+//             sub_storage: {
+//               type: "zip",
+//               sub_storage: {
+//                 type: "local"
+//               }
+//             }
+//           }
+//         }
+//       });
+
+      ///////////////////////////
+      // Memory storage
+      ///////////////////////////
+      return g.run({
+        type: "query",
+        sub_storage: {
+          type: "uuid",
+          sub_storage: {
+            type: "union",
+            storage_list: [{
+              type: "memory"
+            }]
+          }
+        }
+      });
+
+      ///////////////////////////
+      // WebSQL storage
+      ///////////////////////////
+//      return g.run({
+//        type: "query",
+//        sub_storage: {
+//          type: "uuid",
+//          sub_storage: {
+//            "type": "websql",
+//            "database": "test"
+//          }
+//        }
+//      });
+
+      ///////////////////////////
+      // IndexedDB storage
+      ///////////////////////////
+//       return g.run({
+//         type: "query",
+//         sub_storage: {
+//           type: "uuid",
+//           sub_storage: {
+//             "type": "indexeddb",
+//             "database": "test"
+//           }
+//         }
+//       });
+
+      ///////////////////////////
+      // DAV storage
+      ///////////////////////////
+//       return g.run({
+//         type: "query",
+//         sub_storage: {
+//           type: "uuid",
+//           sub_storage: {
+//             type: "drivetojiomapping",
+//             sub_storage: {
+//               "type": "dav",
+//               "url": "DAVURL",
+//               "basic_login": btoa("LOGIN:PASSWD")
+//             }
+//           }
+//         }
+//       });
+
+      ///////////////////////////
+      // Dropbox storage
+      ///////////////////////////
+//       return g.run({
+//         type: "query",
+//         sub_storage: {
+//           type: "uuid",
+//           sub_storage: {
+//             type: "drivetojiomapping",
+//             sub_storage: {
+//               "type": "dropbox",
+//               "access_token" : "TOKEN",
+//               "root" : "dropbox"
+//             }
+//           }
+//         }
+//       });
+
+      ///////////////////////////
+      // Qiniu storage
+      ///////////////////////////
+//       return g.run({
+//         type: "query",
+//         sub_storage: {
+//           type: "uuid",
+//           sub_storage: {
+//             "type": "qiniu",
+//             "bucket": "BUCKET",
+//             "access_key": "ACCESSKEY",
+//             "secret_key": "SECRETKEY"
+//           }
+//         }
+//       });
+
+      ///////////////////////////
+      // Replicate storage
+      ///////////////////////////
+//       return g.run({
+//         type: "query",
+//         sub_storage: {
+//           type: "uuid",
+//           sub_storage: {
+//             type: "replicate",
+//             local_sub_storage: {
+//               type: "memory"
+//             },
+//             remote_sub_storage: {
+//               "type": "memory"
+//             }
+//           }
+//         }
+//       });
+
+      ///////////////////////////
+      // Crypt storage
+      ///////////////////////////
+      // return g.run({
+      //   type: "query",
+      //   sub_storage: {
+      //     type: "uuid",
+      //     sub_storage: {
+      //       type: "crypt",
+      //       key: {"alg": "A256GCM", "ext": true,
+      //             "k": "seeaLzpu8dHG07bO2ANH2GywbTqs_zrs4Vq8zmtYeE4",
+      //             "key_ops": ["encrypt", "decrypt"], "kty": "oct"},
+      //       sub_storage: {
+      //         type: "indexeddb",
+      //         database: "test427"
+      //       }
+      //     }
+      //   }
+      // });
+
 
     })
     .declareMethod('run', function (jio_options) {
 
-      var jio;
+      test('Test "' + jio_options.type + '"scenario', function () {
+        var jio;
+        stop();
+        expect(14);
 
-      try {
-        jio = jIO.createJIO(jio_options);
-      } catch (error) {
-        console.error(error.stack);
-        console.error(error);
-        throw error;
-      }
-
-      // Try to fetch inexistent document
-      jio.get("inexistent")
-        .fail(function (error) {
+        try {
+          jio = jIO.createJIO(jio_options);
+        } catch (error) {
+          console.error(error.stack);
           console.error(error);
-          if (error.status_code !== 404) {
-            throw error;
-          }
-          equal(error.status_code, 404, "404 if inexistent");
+          throw error;
+        }
 
-          // Post a document without ID
-          return jio.post({"title": "I don't have ID éà&\n"});
-        })
-        .then(function (doc_id) {
-          ok(doc_id, "Document without ID created (" + doc_id + ")");
-          // Fetch the newly created document
-          return RSVP.all([
-            jio.get(doc_id),
-            doc_id
-          ]);
-        })
-        .then(function (result_list) {
-          var doc = result_list[0],
-            doc_id = result_list[1];
-          deepEqual(doc, {"title": "I don't have ID éà&\n"},
-                    "Document correctly fetched");
-          // Remove the doc
-          return jio.remove(doc_id);
-        })
-        .then(function (doc_id) {
-          ok(doc_id, "Document removed");
-          // Create some documents to check allDocs
-          return RSVP.all([
-            jio.put("id1", {"title": "1 ID", "int_index": 1}),
-            jio.put("id2", {"title": "2 ID", "int_index": 2}),
-            jio.put("id3", {"title": "3 ID", "int_index": 3})
-          ]);
-        })
-        .then(function (all_doc_id) {
-          equal(all_doc_id[0], "id1", "Document 1 correctly created");
-          equal(all_doc_id[1], "id2", "Document 2 correctly created");
-          equal(all_doc_id[2], "id3", "Document 3 correctly created");
+        // Try to fetch inexistent document
+        jio.get("inexistent")
+          .fail(function (error) {
+            console.error(error);
+            if (error.status_code !== 404) {
+              throw error;
+            }
+            equal(error.status_code, 404, "404 if inexistent");
+
+            // Post a document without ID
+            return jio.post({"title": "I don't have ID éà&\n"});
+          })
+          .then(function (doc_id) {
+            ok(doc_id, "Document without ID created (" + doc_id + ")");
+            // Fetch the newly created document
+            return RSVP.all([
+              jio.get(doc_id),
+              doc_id
+            ]);
+          })
+          .then(function (result_list) {
+            var doc = result_list[0],
+              doc_id = result_list[1];
+            deepEqual(doc, {"title": "I don't have ID éà&\n"},
+                      "Document correctly fetched");
+            // Remove the doc
+            return jio.remove(doc_id);
+          })
+          .then(function (doc_id) {
+            ok(doc_id, "Document removed");
+            // Create some documents to check allDocs
+            return RSVP.all([
+              jio.put("id1", {"title": "1 ID", "int_index": 1}),
+              jio.put("id2", {"title": "2 ID", "int_index": 2}),
+              jio.put("id3", {"title": "3 ID", "int_index": 3})
+            ]);
+          })
+          .then(function (all_doc_id) {
+            equal(all_doc_id[0], "id1", "Document 1 correctly created");
+            equal(all_doc_id[1], "id2", "Document 2 correctly created");
+            equal(all_doc_id[2], "id3", "Document 3 correctly created");
 
 //             // Default allDocs call
 //             return jio.allDocs();
@@ -94,313 +248,94 @@
 //               }
 //             }, "default allDocs OK");
 
-          // Filter the result
-          console.log("START ALLDOCS");
-          return jio.allDocs({
-            query: 'title: "2 ID"',
-            select_list: ["int_index"]
-          });
-        })
-        .then(function (result) {
-          deepEqual(result, {
-            data: {
-              rows: [{
-                doc: {},
-                id: "id2",
-                value: {int_index: 2}
-              }],
-              total_rows: 1
-            }
-          }, "filter allDocs OK");
-
-          // XXX Check include docs, sort, limit, select
-        })
-
-        .then(function () {
-          return jio.getAttachment("inexistent", "enclosure")
-            .fail(function (error) {
-              equal(error.status_code, 404, "404 if inexistent");
-              console.log(error);
+            // Filter the result
+            console.log("START ALLDOCS");
+            return jio.allDocs({
+              query: 'title: "2 ID"',
+              select_list: ["int_index"]
             });
-        })
+          })
+          .then(function (result) {
+            deepEqual(result, {
+              data: {
+                rows: [{
+                  doc: {},
+                  id: "id2",
+                  value: {int_index: 2}
+                }],
+                total_rows: 1
+              }
+            }, "filter allDocs OK");
 
-        .then(function () {
-          return jio.put("foo❤/test.txt", {});
-        })
+            // XXX Check include docs, sort, limit, select
+          })
 
-        .then(function () {
-          return jio.putAttachment(
-            "foo❤/test.txt",
-            "enclosure",
-            new Blob(["fooé\nbar测试四😈"], {type: "text/plain"})
-          );
-        })
+          .then(function () {
+            return jio.getAttachment("inexistent", "enclosure")
+              .fail(function (error) {
+                equal(error.status_code, 404, "404 if inexistent");
+                console.log(error);
+              });
+          })
 
-        .then(function () {
-          ok(true, "Attachment stored");
-          return jio.getAttachment("foo❤/test.txt", "enclosure");
-        })
+          .then(function () {
+            return jio.put("foo❤/test.txt", {});
+          })
 
-        .then(function (blob) {
-          return jIO.util.readBlobAsText(blob);
-        })
+          .then(function () {
+            return jio.putAttachment(
+              "foo❤/test.txt",
+              "enclosure",
+              new Blob(["fooé\nbar测试四😈"], {type: "text/plain"})
+            );
+          })
 
-        .then(function (result) {
-          equal(result.target.result, "fooé\nbar测试四😈", "Attachment correctly fetched");
-          return jio.get("foo❤/test.txt");
+          .then(function () {
+            ok(true, "Attachment stored");
+            return jio.getAttachment("foo❤/test.txt", "enclosure");
+          })
 
-        })
-        .then(function (doc) {
-          deepEqual(doc, {}, "Document correctly fetched");
+          .then(function (blob) {
+            return jIO.util.readBlobAsText(blob);
+          })
 
-          return jio.allAttachments("foo❤/test.txt");
-        })
-        .then(function (doc) {
-          deepEqual(doc, {
-            enclosure: {}
-          },
-            "Attachment list correctly fetched");
+          .then(function (result) {
+            equal(result.target.result, "fooé\nbar测试四😈", "Attachment correctly fetched");
+            return jio.get("foo❤/test.txt");
 
-          return jio.removeAttachment("foo❤/test.txt", "enclosure");
-        })
+          })
+          .then(function (doc) {
+            deepEqual(doc, {}, "Document correctly fetched");
 
-        .then(function () {
-          ok("Attachment removed");
-        })
+            return jio.allAttachments("foo❤/test.txt");
+          })
+          .then(function (doc) {
+            deepEqual(doc, {
+              enclosure: {}
+            },
+              "Attachment list correctly fetched");
 
-        .then(function () {
-          return jio.repair();
-        })
+            return jio.removeAttachment("foo❤/test.txt", "enclosure");
+          })
 
-        .fail(function (error) {
-          console.error("---");
-          console.error(error.stack);
-          console.error(error);
-          ok(false, error);
-        })
-        .always(function () {
-          start();
-        });
+          .then(function () {
+            ok("Attachment removed");
+          })
+
+          .then(function () {
+            return jio.repair();
+          })
+
+          .fail(function (error) {
+            console.error("---");
+            console.error(error.stack);
+            console.error(error);
+            ok(false, error);
+          })
+          .always(function () {
+            start();
+          });
+      });
     });
 
-  module("scenario");
-  ///////////////////////////
-  // Memory storage
-  ///////////////////////////
-  test('Test Memory Storage scenario', function () {
-    stop();
-    expect(14);
-    return global.defer.promise
-      .then(function () {
-        return global.gadget.run({
-          type: "query",
-          sub_storage: {
-            type: "uuid",
-            sub_storage: {
-              type: "union",
-              storage_list: [{
-                type: "memory"
-              }]
-            }
-          }
-        });
-      });
-  });
-  ///////////////////////////
-  // Local storage
-  ///////////////////////////
-  test('Test Local Storage scenario', function () {
-    stop();
-    expect(14);
-    return global.defer.promise
-      .then(function () {
-        return global.gadget.run({
-          type: "query",
-          sub_storage: {
-            type: "uuid",
-            sub_storage: {
-              type: "document",
-              document_id: "/",
-              sub_storage: {
-                type: "zip",
-                sub_storage: {
-                  type: "local"
-                }
-              }
-            }
-          }
-        });
-      });
-  });
-  ///////////////////////////
-  // WebSQL storage
-  ///////////////////////////
-  test('Test WebSQL Storage scenario', function () {
-    stop();
-    expect(14);
-    return global.defer.promise
-      .then(function () {
-        return global.gadget.run({
-          type: "query",
-          sub_storage: {
-            type: "uuid",
-            sub_storage: {
-              "type": "websql",
-              "database": "test"
-            }
-          }
-        });
-      });
-  });
-
-  ///////////////////////////
-  // IndexedDB storage
-  ///////////////////////////
-  test('Test IndexedDB Storage scenario', function () {
-    stop();
-    expect(14);
-    return global.defer.promise
-      .then(function () {
-        return global.gadget.run({
-          type: "query",
-          sub_storage: {
-            type: "uuid",
-            sub_storage: {
-              "type": "indexeddb",
-              "database": "test"
-            }
-          }
-        });
-      });
-  });
-
-  ///////////////////////////
-  // DAV storage
-  ///////////////////////////
-/*
-  test('Test DAV Storage scenario', function () {
-    stop();
-    expect(14);
-    return global.defer.promise
-      .then(function () {
-        return global.gadget.run({
-          type: "query",
-          sub_storage: {
-            type: "uuid",
-            sub_storage: {
-              type: "drivetojiomapping",
-              sub_storage: {
-                "type": "dav",
-                "url": "DAVURL",
-                "basic_login": btoa("LOGIN:PASSWD")
-              }
-            }
-          }
-        });
-      });
-  });
-*/
-  ///////////////////////////
-  // Dropbox storage
-  ///////////////////////////
-/*
-  test('Test Dropbox Storage scenario', function () {
-    stop();
-    expect(14);
-    return global.defer.promise
-      .then(function () {
-        return global.gadget.run({
-          type: "query",
-          sub_storage: {
-            type: "uuid",
-            sub_storage: {
-              type: "drivetojiomapping",
-              sub_storage: {
-                "type": "dropbox",
-                "access_token" : "TOKEN",
-                "root" : "dropbox"
-              }
-            }
-          }
-        });
-      });
-  });
-*/
-  ///////////////////////////
-  // Qiniu storage
-  ///////////////////////////
-/*
-  test('Test Qiniu Storage scenario', function () {
-    stop();
-    expect(14);
-    return global.defer.promise
-      .then(function () {
-        return global.gadget.run({
-          type: "query",
-          sub_storage: {
-            type: "uuid",
-            sub_storage: {
-              "type": "qiniu",
-              "bucket": "BUCKET",
-              "access_key": "ACCESSKEY",
-              "secret_key": "SECRETKEY"
-            }
-          }
-        });
-      });
-  });
-*/
-  ///////////////////////////
-  // Replicate storage
-  ///////////////////////////
-  test('Test Replicate Storage scenario', function () {
-    stop();
-    expect(14);
-    return global.defer.promise
-      .then(function () {
-        return global.gadget.run({
-          type: "query",
-          sub_storage: {
-            type: "uuid",
-            sub_storage: {
-              type: "replicate",
-              local_sub_storage: {
-                type: "memory"
-              },
-              remote_sub_storage: {
-                "type": "memory"
-              }
-            }
-          }
-        });
-      });
-  });
-
-  ///////////////////////////
-  // Crypt storage
-  ///////////////////////////
-  test('Test Crypt Storage scenario', function () {
-    stop();
-    expect(14);
-    return global.defer.promise
-      .then(function () {
-        return global.gadget.run({
-          type: "query",
-          sub_storage: {
-            type: "uuid",
-            sub_storage: {
-              type: "crypt",
-              key: {"alg": "A256GCM", "ext": true,
-                    "k": "seeaLzpu8dHG07bO2ANH2GywbTqs_zrs4Vq8zmtYeE4",
-                    "key_ops": ["encrypt", "decrypt"], "kty": "oct"},
-              sub_storage: {
-                type: "indexeddb",
-                database: "test427"
-              }
-            }
-          }
-        });
-      });
-  });
-
-}(window, QUnit, jIO, rJS, RSVP));
+}(window, QUnit, jIO, rJS));
