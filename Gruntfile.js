b189e4559855c1c9efc0629152218867a4028319
VincentB <vincent.bechu@nexedi.com>
2017-09-20 17:53:58 +0200

    [dateupdater] add storage to gruntfile and test

--- Gruntfile.js
+++ Gruntfile.js
@@ -181,7 +181,9 @@ module.exports = function (grunt) {
           'src/jio.storage/indexeddbstorage.js',
           'src/jio.storage/cryptstorage.js',
           'src/jio.storage/websqlstorage.js',
-          'src/jio.storage/fbstorage.js'
+          'src/jio.storage/fbstorage.js',
+          'src/jio.storage/dateupdaterstorage.js'
+
         ],
         dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.js'
 //         dest: 'jio.js'
