d5b1a039c6e7f08a89effb1eaf0677747c1b309d
Guillaume Royer <guillaume.royer@clearroad.io>
2018-08-29 09:22:54 +0100

    fix(node): support Blob in XMLHttpRequest

--- src/node/jio.js
+++ src/node/jio.js
@@ -1,1 +1,14 @@
-  var jIO = window.jIO;
+var jIO = window.jIO;
+
+// Blob is not supported by xhr2, so convert to ArrayBuffer instead
+var originalAjax = jIO.util.ajax;
+jIO.util.ajax = function ajax(param) {
+  if (param.data instanceof Blob) {
+    return jIO.util.readBlobAsArrayBuffer(param.data).then(function (data) {
+      param.data = data.target.result;
+      return originalAjax(param);
+    });
+  }
+
+  return originalAjax(param);
+};
